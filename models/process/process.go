486c5e47f8d51bf2a6807844cdf8916a8028de54
Carl Turechek <carl@siteraiser.com>
2024-06-14 16:20:53 -0400

    Update process.go

--- models/process/process.go
+++ models/process/process.go
@@ -730,6 +730,9 @@ func createTransferList() ([]rpc.Transfer, []ResponseTx) {
 	}
 
 	for i, responseTx := range pending_orders {
+		if responseTx.Amount == 0 || responseTx.Port == 0 {
+			continue 1;
+		}
 		settings := getIASettings(responseTx.Amount, responseTx.Port)
 		switch responseTx.Type {
 		case "physical_sale", "digital_sale":
